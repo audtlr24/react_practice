878b2953c358582f9a3536d2d386b99b04765d38
audtlr24 <audtlr24@gmail.com>
2022-02-06 18:08:40 +0900

    add api call using redux-thunk

---
+++ learn-redux-middleware/src/components/Counter.js
@@ -0,0 +1,21 @@
+import React, {useCallback} from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import {increaseAsync,decreaseAsync} from '../modules/counter'
+
+const Counter = ()=>{
+
+    const number = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(()=>dispatch(increaseAsync()),[dispatch]);
+    const onDecrease = useCallback(()=>dispatch(decreaseAsync()),[dispatch]);
+
+    return(
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
+        </div>
+    )
+};
+
+export default Counter;
