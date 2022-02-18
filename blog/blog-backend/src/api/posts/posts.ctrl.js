79ef128ea92ac4d00d4571d899ffe142f8b73ca8
audtlr24 <audtlr24@gmail.com>
2022-02-18 19:53:46 +0900

    feat: blog-backend add login, post query, cookies

--- blog/blog-backend/src/api/posts/posts.ctrl.js
+++ blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -4,10 +4,29 @@ import Joi from '@hapi/joi';
 
 const {ObjectId} = mongoose.Types;
 
-export const checkObjectId = (ctx,next)=>{
+export const getPostById = async (ctx,next)=>{
     const {id} = ctx.params;
     if(!ObjectId.isValid(id)){
         ctx.status = 400;
+        return;
+    }
+    try{
+        const post = await Post.findById(id);
+        if(!post){
+            ctx.status = 404;
+            return;
+        }
+        ctx.state.post = post;
+        return next();
+    }catch(e){
+        ctx.throw(500,e);
+    }
+};
+
+export const checkOwnPost = (ctx,next)=>{
+    const {user,post} = ctx.state;
+    if(post.user._id.toString() !== user._id){
+        ctx.status = 403;
         return;
     }
     return next();
@@ -35,6 +54,7 @@ export const write = async ctx =>{
         title,
         body,
         tags,
+        user: ctx.state.user,
     });
     try{
         await post.save();
@@ -43,7 +63,9 @@ export const write = async ctx =>{
         ctx.throw(500,e);
     }
 };
-
+/*
+    GET /api/posts?username=&tag=&page=
+*/
 export const list = async ctx=>{
 
     const page = parseInt(ctx.query.page || '1',10);
@@ -53,13 +75,19 @@ export const list = async ctx=>{
         return;
     }
 
+    const {tag,username} = ctx.query;
+    const query = {
+        ...(username ? {'user.username':username} : {}),
+        ...(tag ? {tags: tag} : {}),
+    };
+
     try{
-        const posts = await Post.find()
+        const posts = await Post.find(query)
             .sort({_id: -1})
             .limit(10)
             .skip((page-1)*10)
             .exec();
-        const postCount = await Post.countDocuments().exec();
+        const postCount = await Post.countDocuments(query).exec();
         ctx.set('Last-page', Math.ceil(postCount/10));
         ctx.body = posts
         .map(post=>post.toJSON())
@@ -73,18 +101,8 @@ export const list = async ctx=>{
     }
 };
 
-export const read = async ctx=>{
-    const {id} = ctx.params;
-    try{
-        const post = await Post.findById(id).exec();
-        if(!post){
-            ctx.status = 404;
-            return;
-        }
-        ctx.body = post;
-    }catch(e){
-        ctx.throw(500,e);
-    }
+export const read = ctx=>{
+    ctx.body = ctx.state.post;
 };
 
 export const remove = async ctx=>{
